4d26a2b9510311950cd168a195a6bae7bfc0e7fb
DPF0190 <domenick.fumai@gmail.com>
2016-07-21 00:58:14 +0000

    main

---
+++ main.py
@@ -0,0 +1,30 @@
+def filter_custom(l, f):
+    '''filter a list using a function
+    return a new list that contains all the elements of e of l for which f(e) is True
+    :param l: a list
+    :param f: a function that takes on one argument and returns either True or False'''
+
+    pass
+
+def map_custom(l ,f):
+    '''map  a list using a function
+    return a new list that applies f(e) for every element in l
+
+    :param l: a list
+    :param f: a function that takes on one argument and returns a value'''
+
+    pass
+
+
+def reduce_custom(l, f, starting_value):
+    '''reduce a list using a reducer function and a starting value
+    return a single value that applies f(v, e) for every e in l from left to right. the initial value for v should be starting_value, and subsequent values should be the previously calculated value from f(v, e)
+
+:param l: a list
+param f: a function that takes one argument and returns a value :para starting_value: the begginning value for the reducer function computation'''
+
+
+
+
+    pass
+
