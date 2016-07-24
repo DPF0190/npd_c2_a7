197f71a26aaad8b80fe74366193468dbf1dcb210
DPF0190 <domenick.fumai@gmail.com>
2016-07-24 17:59:50 +0000

    main.py

--- main.py
+++ main.py
@@ -4,27 +4,46 @@ def filter_custom(l, f):
     :param l: a list
     :param f: a function that takes on one argument and returns either True or False'''
 
-    pass
+
+    return [e for e in l if f(e)]
+
 
 def map_custom(l ,f):
+
     '''map  a list using a function
     return a new list that applies f(e) for every element in l
 
     :param l: a list
     :param f: a function that takes on one argument and returns a value'''
 
-    pass
+    new_list = []
+    for e in l:
+        new_list.append(f(e))
+    return new_list
 
-
-def reduce_custom(l, f, starting_value):
+def reduce_custom(l, x, starting_value):
     '''reduce a list using a reducer function and a starting value
     return a single value that applies f(v, e) for every e in l from left to right. the initial value for v should be starting_value, and subsequent values should be the previously calculated value from f(v, e)
 
 :param l: a list
 param f: a function that takes one argument and returns a value :para starting_value: the begginning value for the reducer function computation'''
 
+    left_argument = starting_value
+    for right_argument in l:
+        left_argument = f(left_argument, right_argument)
+    return left_argument
 
 
 
-    pass
+
+if __name__ == '__main__':
+    l = [7,8,9]
+    f = lambda x: x * 10
+    print(map_custom(l, f))
+
+print(filter_custom(l, f))
+print(new_list_filt(l, f))
+print(reduce_custom([1,2,3,17], f, 0))
+
+
 
